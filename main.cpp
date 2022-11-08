1d3f21cac3ab7b27f75f28abccb47206da9c8854
Pavel Gerasin <374007@gmail.com>
2022-11-08 17:29:04 +0700

    08.11.2022

--- main.cpp
+++ main.cpp
@@ -1,22 +1,39 @@
 #include <iostream>
 
-bool valid_table (std::string line1, std::string line2, std::string line3) {
-    if (line1.length() != 3 && line2.length() != 3 && line3.length() !=3 ) return false;
-    for (int i = 0; i < 2; i++) {
-        if (line1[i] != 'x' && line1[i] != 'o' && line1[i] != '.'
-            || line2[i] != 'x' && line2[i] != 'o' && line2[i] != '.'
-            || line3[i] != 'x' && line3[i] != 'o' && line3[i] != '.') {
+bool valid_table (std::string tictactoe) {
+    if (tictactoe.length() != 9 ) return false;
+    for (int i = 0; i < 9; i++) {
+        if (tictactoe[i] != 'x' && tictactoe[i] != 'o' && tictactoe[i] != '.')
+             {
             return false;
         }
     }
     return true;
 }
+
+int count_simbols (std::string tictactoe, char smbl )  {
+    int count = 0;
+    for (int i = 0; i < 9; i++) {
+        if (tictactoe[i] == smbl) count++;
+    }
+   return count;
+}
+
+char simbol (std::string tictactoe, int x, int y) {
+
+}
+
 int main() {
     std::string line1, line2, line3;
     std::cout << "Input three line:" << std::endl;
     std::cin >> line1;
     std::cin >> line2;
     std::cin >> line3;
-    std::cout<<valid_table(line1,line2,line3);
+    std::string tictactoe = line1 + line2 + line3;
+    std::cout<<valid_table(tictactoe)<<"-\n";
+    std::cout<<count_simbols(tictactoe,'x')<<"-\n";
+    std::cout<<count_simbols(tictactoe,'o')<<"-\n";
+    std::cout<<count_simbols(tictactoe,'.')<<"-\n";
+
     return 0;
 }
